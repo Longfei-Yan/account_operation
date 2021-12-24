3d7576e289842be86c567d39a45ef5d72602238a
longfei <1371986272@qq.com>
2021-12-24 17:11:43 +0800

    lgog名随机

--- app/Admin/Controllers/LicenseController.php
+++ app/Admin/Controllers/LicenseController.php
@@ -67,7 +67,7 @@ class LicenseController extends AdminController
             $form->text('title');
             $form->text('address');
             $form->image('photo')->move('images/license')->autoUpload();
-            $form->image('logo')->move('images/logo')->autoUpload();
+            $form->image('logo')->move('images/logo')->uniqueName()->autoUpload();
             $form->display('created_at');
             $form->display('updated_at');
         });
