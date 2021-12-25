4fedced3a0e1d819ec64deb757091047e2198de5
longfei <1371986272@qq.com>
2021-12-25 13:43:51 +0800

    hotx

--- resources/views/theme001.blade.php
+++ resources/views/theme001.blade.php
@@ -47,25 +47,8 @@
         </ul>
     </nav>
 </header>
-<!-- banner -->
-<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-    </ol>
-
+    <!-- banner -->
     <img src="{{ URL::asset('uploads/'.$license['banner']) }}" class="d-block w-100" alt="...">
-    <div class="carousel-caption d-none d-md-block">
-
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
     <!-- banner -->
 
     <!-- about -->
