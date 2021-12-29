e0d0bdf1a56c129ebc4be38a87cd973fe9a8bc5e
longfei <1371986272@qq.com>
2021-12-29 16:20:18 +0800

    hotx

--- resources/lang/zh_CN/site.php
+++ resources/lang/zh_CN/site.php
@@ -10,6 +10,7 @@ return [
         'goods_id' => '商品',
         'article_id' => '文章',
         'template_id' => '模板',
+        'email_id' => '邮箱',
         'remark' => '备注',
         'state' => '状态',
     ],
